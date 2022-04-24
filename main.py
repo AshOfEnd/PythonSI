58cc283c9521c1a17779ede605fd50434f125f32
wloda <AshOfEnd@wp.pl>
2022-04-24 21:40:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images/255, testing_images/255
+
+class_names = ['Dog','Cat','Deer','Plane','Car','Bird','Frog','Horse','Ship','Truck']
+
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+plt.show()
