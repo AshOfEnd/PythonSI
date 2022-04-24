4da3fef16ada684b7fc5f2e118aa126dc2b17912
wloda <AshOfEnd@wp.pl>
2022-04-24 23:28:07 +0200

    step 2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tensorflow.keras import datasets, layers, models
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 training_images, testing_images = training_images/255, testing_images/255
 
-class_names = ['Dog','Cat','Deer','Plane','Car','Bird','Frog','Horse','Ship','Truck']
+class_names = ['Plane','Car','Bird','Cat','Deer','Dog','Frog','Ship','Ship','Truck']
 
 for i in range(16):
     plt.subplot(4,4,i+1)
@@ -14,4 +14,33 @@ for i in range(16):
     plt.imshow(training_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[training_labels[i][0]])
 
-plt.show()
+# plt.show()
+
+training_images = training_images[:200000]
+training_labels = training_labels[:200000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+model=models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10,activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+model.save('Classifier.model')
+
+model = models.load_model()
+
+
